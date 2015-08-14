ff795bd8b46e9866d1ce744296cf41f4ed2b59a7
Toma Apostolov <tomaster@gmail.com>
2015-08-14 12:04:50 -0700

    pass specs for returning the ID of a single word

--- lib/words.rb
+++ lib/words.rb
@@ -1,13 +1,39 @@
 class Word
-	@@words = []
+  @@words = []
 
-	define_method(:initialize) do |single_word|
-		@single_word = single_word
-		@id = @@words.length().+(1)
-	end
+  define_method(:initialize) do |single_word|
+    @single_word = single_word
+    @id = @@words.length().+(1)
+    
+  end
 
-	define_method(:single_word) do 
-		@single_word
-	end
-		
+  define_method(:single_word) do
+    @single_word
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |single_word|
+      if single_word.id().eql?(id)
+        found_word = single_word
+      end
+    end
+    found_word
+  end
 end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -1,10 +1,54 @@
 require('rspec')
 require('words')
-# require('definitions')
+
+describe(Word) do
+  before() do
+    Word.clear()
+  end
+end
 
 describe("#single_word") do
 	it('returns a single word from the Words list') do 
 		test_word = Word.new("Ruby")
 		expect(test_word.single_word).to(eq("Ruby"))
 	end
+end
+
+describe("#id") do
+    it("returns the id of the single word") do
+      test_word = Word.new("Ruby")
+      expect(test_word.id()).to(eq(1))
+  end
+end
+
+  describe('.all') do
+    it("words list is empty at first") do
+      expect(Word.all()).to(eq([]))
+  end
+end
+
+describe('#save') do
+    it("adds a word to the array of saved words") do
+      test_word = Word.new("Ruby")
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+  end
+end
+
+describe(".clear") do
+    it("empties all saved words") do
+      Word.new("Ruby").save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+  end
+end
+
+describe(".find") do
+    it("returns a word by its id number") do
+      test_word = Word.new("Ruby")
+      test_word.save()
+      test_word2 = Word.new("Code")
+      test_word2.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+  end
 end
